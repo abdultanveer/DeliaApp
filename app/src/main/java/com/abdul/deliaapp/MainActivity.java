ffff43222bab4c3e53fcb00466c4bca5662f17b9
abdul <abdul.tanveer@gmail.com>
2021-07-03 07:43:04 +0530

    onfocuschange listener

--- app/src/main/java/com/abdul/deliaapp/MainActivity.java
+++ app/src/main/java/com/abdul/deliaapp/MainActivity.java
@@ -7,17 +7,28 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnFocusChangeListener {
+    EditText nameEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //activity_main was inflated
+         nameEditText = findViewById(R.id.etName); //getting a handle from the inflated xml
+        nameEditText.setOnFocusChangeListener(this);
+
     }
 
     public void clickHandler(View view) {
-        EditText nameEditText = findViewById(R.id.etName); //getting a handle from the inflated xml
         String name = nameEditText.getText().toString();
         Toast.makeText(this, "welcome to android"+name, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean hasFocus) {
+        if(!hasFocus){
+            Toast.makeText(this, "this name is already taken", Toast.LENGTH_SHORT).show();
+            nameEditText.setText("");
+        }
+
     }
 }
